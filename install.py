84c6d27ab5757fc20122f3dcf0d51d84adf189ce
Blinc <65546512+Blinc13@users.noreply.github.com>
2020-10-19 16:13:55 +0300

    Add files via upload

---
+++ install.py
@@ -0,0 +1,13 @@
+import os
+try:
+  os.system("pip install yapi")
+except:
+  pass
+try:
+  os.system("pip install colorama")
+except:
+  pass
+try:
+  os.system("pip install youtube-dl")
+except:
+  pass
